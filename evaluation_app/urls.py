52ea9c9d516a25fecb30e6d5250e5239f2583b0e
BelieveMko <mukobelieve@gmail.com>
2026-02-22 15:19:22 +0200

    Implement Project CRUD Endpoints with ModelViewSet and url configuration

--- evaluation_app/urls.py
+++ evaluation_app/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from .views import NewUserRegistrationView
+from rest_framework.routers import DefaultRouter
+from .views import ProjectViewCRUD
+
+
+router = DefaultRouter()
+router.register(r'projects', ProjectViewCRUD, basename='comapanyprojects')
+
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('register/', NewUserRegistrationView.as_view(), name='new_user_register'),
+    
 ]
--- evaluation_app/views.py
+++ evaluation_app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import NewUserRegistrationSerializer
 from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from .models import Project
+from .serializers import ProjectSerializer
+
 
 User = get_user_model()
 
@@ -9,3 +13,8 @@ class NewUserRegistrationView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = NewUserRegistrationSerializer
 
+
+
+class ProjectViewCRUD(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
