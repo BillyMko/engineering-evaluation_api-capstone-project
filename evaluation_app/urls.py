e3b217344d06ce9669cbfe43ecbf404b0d6177f5
BelieveMko <mukobelieve@gmail.com>
2026-02-27 13:36:02 +0200

    Register Submission endpoints

--- evaluation_app/urls.py
+++ evaluation_app/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from .views import NewUserRegistrationView
 from rest_framework.routers import DefaultRouter
-from .views import ProjectViewCRUD
+from .views import ProjectViewCRUD, SubmissionViewCRUD
 
 
 router = DefaultRouter()
 router.register(r'projects', ProjectViewCRUD, basename='comapanyprojects')
+router.register(r'submissions', SubmissionViewCRUD, basename='projectsubmissions')
 
 
 urlpatterns = [
