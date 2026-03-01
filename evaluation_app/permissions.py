5732a68f38ec60fd519bc77ee98efd114e32d447
BelieveMko <mukobelieve@gmail.com>
2026-03-01 18:16:56 +0200

    Add Submission and Evaluation permissions in permissions.py

--- evaluation_app/permissions.py
+++ evaluation_app/permissions.py
@@ -11,3 +11,32 @@ class IsProjectOwnerIfNotReadOnly(BasePermission):
         if request.method in ["GET", "HEAD", "OPTIONS"]:
             return True
         return obj.created_by == request.user
+    
+class IsStudentThenSubmit(BasePermission):
+    def has_permission(self, request, view):
+        if request.user.role == "company":
+            return False
+        
+        if request.method == "POST":
+            return request.user.is_authenticated and request.user.role == "student"
+        return True
+
+class IsSubmissionTheOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in ["GET", "HEAD", "OPTIONS"]:
+            return True
+        return obj.student == request.user
+
+
+class IsCompanyThenEvaluate(BasePermission):
+    def has_permission(self, request, view):
+        if request.method == "POST":
+            return request.user.is_authenticated and request.user.role == "company"
+        return True
+    
+class IsProjectOwnerThenEvaluate(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in ["GET", "HEAD", "OPTIONS"]:
+            return True
+        return obj.submission.project.company == request.user
+
