bd297fb08ae740990a21731fd7954b1b7d5888b5
BelieveMko <mukobelieve@gmail.com>
2026-03-08 21:45:18 +0200

    Chaged MySQL database to db.sqlite3 database

--- engineering_evaluation_api/settings.py
+++ engineering_evaluation_api/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'engineering_evaluation_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'engineering_evaluation_api_db',
-        'USER': 'believemukomberanwa',
-        'PASSWORD': 'mypassword1956',
-        'HOST': 'localhost',
-        'PORT': '3306',
+         'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
